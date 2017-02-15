7ce3eb118611d9a7466eefca790119e8e85c117b
just nobody <just nobody>
2017-02-15 00:24:54 -0500

    added UI to display the current "target"

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,34 @@
+const VIEW_WIDTH = 960
+const VIEW_HEIGHT = 540
+
 const KEY_LEFT = 37
 const KEY_RIGHT = 39
 const KEY_UP = 38
 const KEY_DOWN = 40
 
 function setup() {
-  let game = new PIXI.Application(960, 540)
+  let game = new PIXI.Application(VIEW_WIDTH, VIEW_HEIGHT)
   document.body.appendChild(game.view)
 
   let background = game.stage.addChild(PIXI.Sprite.fromImage('resources/sky.png'))
-  background.width = game.view.width
-  background.height = game.view.height
+  background.width = VIEW_WIDTH
+  background.height = VIEW_HEIGHT
 
-  // let player = new Player(100, 300)
-  // game.stage.addChild(player.sprite)
+  let targetDisplay = game.stage.addChild(new PIXI.Container())
+  let targetText = targetDisplay.addChild(new PIXI.Text('Touch:', { fill: 'white', stroke: 'black', strokeThickness: 3, fontSize: 40 }))
+  let targetSprite = targetDisplay.addChild(PIXI.Sprite.fromImage('resources/apple.png'))
 
-  // // let fruits = [] as Fruit[]
+  targetText.anchor.set(0, 0.5)
 
-  // game.ticker.add(dt => {
-  //   player.movingLeft = Keyboard.isDown(KEY_LEFT)
-  //   player.movingRight = Keyboard.isDown(KEY_RIGHT)
-  //   player.update(dt)
-  //   // fruits.forEach(fruit => {
-  //   //   fruit.update(dt)
-  //   // })
-  // })
+  targetSprite.anchor.set(0, 0.5)
+  targetSprite.position.set(targetText.width, 0)
+  targetSprite.scale.set(0.75)
+
+  targetDisplay.pivot.set(targetDisplay.width / 2, targetDisplay.height / 2)
+  targetDisplay.position.set(VIEW_WIDTH / 2, 100)
 }
 
 PIXI.loader
+  .add('resources/apple.png')
   .add('resources/sky.png')
   .load(setup)
