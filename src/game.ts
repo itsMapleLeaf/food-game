8655d3aeef14ad550bd299710d3d34c2d54d9c5a
just nobody <just nobody>
2017-02-15 01:43:56 -0500

    almost there...

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,43 @@
-class Game {
-  start() {
-    let game = new PIXI.Application(VIEW_WIDTH, VIEW_HEIGHT)
-    document.body.appendChild(game.view)
+class Target {
+  image = util.randomItem(FOOD_IMAGES)
+  sprite = PIXI.Sprite.fromImage(this.image)
 
-    let background = game.stage.addChild(PIXI.Sprite.fromImage('resources/sky.png'))
+  constructor() {
+    this.sprite.position.set(VIEW_WIDTH / 2, VIEW_HEIGHT / 2)
+    this.sprite.anchor.set(0.5, 0.5)
+    this.sprite.scale.set(1.2)
+  }
+}
+
+class Game {
+  app = new PIXI.Application(VIEW_WIDTH, VIEW_HEIGHT)
+  levelStage = new PIXI.Container()
+  targets = [] as PIXI.Sprite[]
+  level = 0
+
+  start() {
+    document.body.appendChild(this.app.view)
+
+    let background = PIXI.Sprite.fromImage('resources/sky.png')
     background.width = VIEW_WIDTH
     background.height = VIEW_HEIGHT
 
-    game.stage.addChild(this.createTargetDisplay('resources/apple.png'))
+    this.app.stage.addChild(background)
+    this.app.stage.addChild(this.levelStage)
+
+    this.startNextLevel()
+  }
+
+  startNextLevel() {
+    this.level += 1
+
+    let target = new Target()
+    let targetDisplay = this.createTargetDisplay(target.image)
+
+    let level = this.levelStage
+    level.removeChildren()
+    level.addChild(targetDisplay)
+    level.addChild(target.sprite)
   }
 
   createTargetDisplay(targetImage: string) {
--- src/target.ts
+++
@@ -1,7 +0,0 @@
-class Target {
-  public sprite: PIXI.Sprite
-
-  constructor(public image: string) {
-    this.sprite = PIXI.Sprite.fromImage(image)
-  }
-}
