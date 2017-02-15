5b207907f148369631858e08db709cc7d70ffc5e
just nobody <just nobody>
2017-02-15 02:04:09 -0500

    move score display to separate class

--- src/game.ts
+++ src/game.ts
@@ -33,30 +33,41 @@ class TargetDisplay {
   }
 }
 
-class Game {
-  app = new PIXI.Application(VIEW_WIDTH, VIEW_HEIGHT)
-  level = 0
-  levelStage = new PIXI.Container()
-  score = 0
-  scoreDisplay = new PIXI.Text('Score: 0', {
+class ScoreDisplay {
+  sprite = new PIXI.Text('', {
     fill: 'white',
     stroke: 'black',
     strokeThickness: 3,
     fontSize: 40,
   })
+
+  constructor() {
+    this.sprite.anchor.set(0.5, 0.5)
+    this.sprite.position.set(VIEW_WIDTH / 2, VIEW_HEIGHT * 0.9)
+    this.update(0)
+  }
+
+  update(score: any) {
+    this.sprite.text = 'Score: ' + score.toString()
+  }
+}
+
+class Game {
+  app = new PIXI.Application(VIEW_WIDTH, VIEW_HEIGHT)
+  level = 0
+  levelStage = new PIXI.Container()
+  score = 0
+  scoreDisplay = new ScoreDisplay()
   targets = [] as Target[]
 
   start() {
-    this.scoreDisplay.anchor.set(0.5, 0.5)
-    this.scoreDisplay.position.set(VIEW_WIDTH / 2, VIEW_HEIGHT * 0.9)
-
     let background = PIXI.Sprite.fromImage('resources/sky.png')
     background.width = VIEW_WIDTH
     background.height = VIEW_HEIGHT
 
     this.app.stage.addChild(background)
     this.app.stage.addChild(this.levelStage)
-    this.app.stage.addChild(this.scoreDisplay)
+    this.app.stage.addChild(this.scoreDisplay.sprite)
 
     document.body.appendChild(this.app.view)
 
@@ -77,7 +88,7 @@ class Game {
     target.sprite.interactive = true
     target.sprite.once('pointerdown', (event: PIXI.interaction.InteractionEvent) => {
       this.score += 1
-      this.scoreDisplay.text = 'Score: ' + this.score.toString()
+      this.scoreDisplay.update(this.score)
       this.startNextLevel()
     })
   }
