a88884ba3626bb19b5d392137c26f78417c2a6f8
just nobody <just nobody>
2017-02-15 00:27:19 -0500

    load images, save an array of fruits for later

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 const VIEW_WIDTH = 960
 const VIEW_HEIGHT = 540
 
+const BACKGROUND_IMAGE = 'resources/sky.png'
+const FRUIT_IMAGES = [
+  'resources/apple.png',
+  'resources/banana.png',
+  'resources/cake.png',
+  'resources/cherry.png',
+  'resources/grape.png',
+  'resources/jello.png',
+  'resources/lemon.png',
+  'resources/orange.png',
+  'resources/pear.png',
+  'resources/tomato.png',
+]
+
 const KEY_LEFT = 37
 const KEY_RIGHT = 39
 const KEY_UP = 38
@@ -29,6 +43,6 @@ function setup() {
 }
 
 PIXI.loader
-  .add('resources/apple.png')
-  .add('resources/sky.png')
+  .add(FRUIT_IMAGES)
+  .add(BACKGROUND_IMAGE)
   .load(setup)
